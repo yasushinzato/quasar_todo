ea3e713f521c559858675df4d0d16f6ee143fe42
yasushinzato <ahzinots.uuiyyska@gmail.com>
2020-05-24 16:49:43 +0900

    テキストを全体選択状態にする

--- src/components/Tasks/Modals/shares/ModalTaskName.vue
+++ src/components/Tasks/Modals/shares/ModalTaskName.vue
@@ -8,6 +8,7 @@
       @input="$emit('update:name', $event)"
       color="purple-12"
       autofocus
+      v-select-all
       ref="name"
       label="タスク内容"
       class="col"
@@ -22,7 +23,21 @@
 
 <script>
 export default {
-  props: ["name"]
+  props: ["name"],
+  directives: {
+    // 入力して再選択した場合、テキスト全体を選択状態にする。
+    selectAll: {
+      inserted(el) {
+        // 開発ツールでCSSを確認する。 .のつけ忘れに注意すること
+        let input = el.querySelector(".q-field__native");
+        input.addEventListener("focus", () => {
+          if (input.value.length) {
+            input.select();
+          }
+        });
+      }
+    }
+  }
 };
 </script>
 
